f79abfbc9dc5f79819f85cc269aa6776dc27f5e9
onePone <pwang31@trip.com>
2023-06-15 16:25:12 +0800

    chore: version 0.0.3-beta.13

--- packages/arex-core/src/components/HelpTooltip.tsx
+++ packages/arex-core/src/components/HelpTooltip.tsx
@@ -1,15 +1,11 @@
 import { QuestionCircleOutlined } from '@ant-design/icons';
 import { Tooltip, Typography } from 'antd';
-import React, { FC } from 'react';
+import React, { FC, ReactNode } from 'react';
 
-const HelpTooltip: FC<{ title: string; children: React.ReactNode }> = (props) => (
+const HelpTooltip: FC<{ title: ReactNode; children: React.ReactNode }> = (props) => (
   <>
     <Typography.Text>{props.children}</Typography.Text>
-    <Tooltip
-      title={<Typography.Text>{props.title}</Typography.Text>}
-      placement='top'
-      overlayStyle={{ maxWidth: '200px' }}
-    >
+    <Tooltip title={props.title} placement='top' overlayStyle={{ maxWidth: '200px' }}>
       <QuestionCircleOutlined style={{ marginLeft: '4px' }} />
     </Tooltip>
   </>
